4e3b83ac494bbd69c5d43593087cfbacd2da908c
Jada <jada.fagan@gmail.com>
2020-08-20 10:58:33 -0400

    Add project files.

---
+++ AdvancedCSharp/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdvancedCSharp
+{
+    struct Struct1
+    {
+        //can have properties, methods, and constructors-but cannot be empty(must have values since it is a value type and must be initialized)
+
+    }
+}
---
+++ AdvancedCSharp/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net.NetworkInformation;
+
+namespace AdvancedCSharp
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+
+            int[] arr= { 1, 2, 3, 4, 5 };
+            var aType = 123.45;
+            Console.WriteLine($"THe type of aType is {aType.GetType()}");
+            var aTypeOf = typeof(System.String);
+            Console.WriteLine($"TypeOf System.String is {aTypeOf}");
+            Console.ReadKey();
+            /* typeOf is for creating a type from a class. getType is for programmaticaly finding the type of a variable*/
+
+            var str = "X123";
+            int nbr;
+            int.TryParse(str, out nbr);
+            Console.WriteLine($"Nbr is {nbr}");
+            /*tryparse is good for telling if the string to nbr converted successfully with the out parameter.
+            If a character is in the string it will return 0, if not will return the converted number(why it is bool)*/
+            //Console.ReadKey();
+
+            var n = 0;
+            Add1(n);
+            Console.WriteLine($"N is {n}");
+            Add1Ref(ref n);
+            Console.WriteLine($"N is {n}");
+            //Console.ReadKey();
+            // a behind the scenes copy of n is being passed into Add1, var n is never actually changed
+        }
+
+
+        static void Add1(int i)
+        {
+            i++;
+        }
+        static void Add1Ref(ref int i)
+        {
+            i++;
+        }
+    }
+}
